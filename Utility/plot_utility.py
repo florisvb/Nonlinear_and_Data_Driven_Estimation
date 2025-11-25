d06e6c41e4422c6ba5af2e3d07dd8ddd5536edcd
Floris van Breugel <florisvb@gmail.com>
2025-11-25 10:01:26 -0800

    add markersize option

--- Utility/plot_utility.py
+++ Utility/plot_utility.py
@@ -4,13 +4,13 @@ matplotlib.rc('font', **font)
 
 import matplotlib.pyplot as plt
 
-def plot_tme(t, true, measured, estimated=None, ax=None, label_var='y'):
+def plot_tme(t, true, measured, estimated=None, ax=None, label_var='y', markersize=2):
     if ax is None:
         fig = plt.figure(figsize=(6,4))
         ax = fig.add_subplot(111)
 
     if measured is not None:
-        ax.plot(t, measured, '*', color='blue', label=label_var + ' measured', markersize=0.5)
+        ax.plot(t, measured, '*', color='blue', label=label_var + ' measured', markersize=markersize)
         
     if estimated is not None:
         ax.plot(t, estimated, color='red', label=label_var + ' hat')
